33139ea5575f719d698bba76b595d037e24a162f
Felicity <felicitycraft22@gmail.com>
2023-01-23 16:56:36 +1300

    chore: custom font sizes set for responsive header

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      'base': ['1rem', {
+        lineHeight: '1.5rem',
+        fontWeight: '200',
+        letterSpacing: '0.1em',
+      }],
+      'xl': ['1.25rem', {
+        lineHeight: '1.75rem',
+        fontWeight: '200',
+        letterSpacing: '0.1em',
+      }],
+      '5xl': ['3.4rem', {
+        lineHeight: '1rem',
+        fontWeight: '400',
+      }],
+      '7xl': ['4.5rem', {
+        lineHeight: '2rem',
+        fontWeight: '400',
+      }]
+    },
+    extend: {}
   },
   plugins: [],
 }
