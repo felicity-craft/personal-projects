414e2cebc0d9e50f47ed85226e00b0959498a166
Felicity <felicitycraft22@gmail.com>
2023-01-27 12:34:39 +1300

    chore: extra padding added to mobile view navbar

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationStart, Router, RouterEvent, Event, Navigation } from '@angular/router';
+import { filter, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit {
+  private destroyed$ = new Subject();
   showMenu = false;
-  toggleNavbar(){
-    this.showMenu = !this.showMenu;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    //close nav menu on mobile on navigate
+    //used: https://medium.com/angular-shots/shot-4-how-to-listen-angular-router-events-7a102cca5a80
+    this.router.events.pipe(
+      filter((e: Event): e is RouterEvent => e instanceof NavigationStart),
+      takeUntil(this.destroyed$)).subscribe((e) => {
+        this.showMenu = false   
+      });
   }
 
+  toggleNavbar() {
+    this.showMenu = !this.showMenu;
+  }
 }
